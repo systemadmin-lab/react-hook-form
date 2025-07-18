e48d0f2d5f58d04dc4966aa37a133593501eff40
systemadmin-lab <rejuananik@gmail.com>
2025-07-18 06:56:14 +0600

    Initial commit

--- src/Forms/LogInForm.jsx
+++ src/Forms/LogInForm.jsx
@@ -1,38 +1,47 @@
-import React from 'react'
-import FieldSet from '../Components/FieldSet'
-import Field from '../Components/Field'
+import { useForm } from "react-hook-form";
+import Field from "../Components/Field";
+import FieldSet from "../Components/FieldSet";
 
 const LogInForm = () => {
+  //we are importing hooks from react hook from
+
+  const { register, handleSubmit } = useForm();
+  const submitfrom = (f) => {
+    console.log(f);
+  };
+
   return (
     <div>
-       <form>
-         <FieldSet label="Enter Your LogIn details">
-            <Field label="Email">
-              <input 
-              type='email'
-              name='email'
-              id='email'
-              placeholder='Enter Your email adress'
-              />
-            </Field>
-              <Field label="Password">
-              <input 
-              type='password'
-              name='password'
-              id='Enter Password'
-              placeholder='Enter Password'
-              />
-            </Field>
-            <Field>
-                <button
-                className='text-md text-white text-md cursor-pointer p-1 rounded-lg m-auto bt-purple-500 bg-black'
-                >LogIn</button>
-            </Field>
+      <form onSubmit={handleSubmit(submitfrom)}>
+        <FieldSet label="Enter Your LogIn details">
+          <Field label="Email">
+            <input
+              {...register("email")}
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter Your email adress"
+            />
+          </Field>
 
+          <Field label="Password">
+            <input
+              {...register("password")}
+              type="password"
+              name="password"
+              id="Enter Password"
+              placeholder="Enter Password"
+            />
+          </Field>
+          <Field>
+            <button className="text-md text-white text-md cursor-pointer p-1 rounded-lg m-auto bt-purple-500 bg-black">
+              LogIn
+            </button>
+          </Field>
         </FieldSet>
-       </form>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LogInForm
+export default LogInForm;
